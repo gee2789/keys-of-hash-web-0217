6aca605f82ad72160c730f0ea6e120e83a6c1938
Gee Li <geeyung.li@gmail.com>
2017-02-05 04:44:20 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    #splat(*) captures whatever other arguments you pass into the method.
+    #the splat is required for when you're passing multiple keys
+    collect {|key,value| arguments.include?(value)? key:nil}.compact
+      #collect enumerator pulls and passes array
+        #the block compares whether the arguments(array) contains any matches to argument
+          #the "?" is an if true statement.  If true it passes the key: value.
+            #compact cleans up the collect array without any nils
   end
 end
